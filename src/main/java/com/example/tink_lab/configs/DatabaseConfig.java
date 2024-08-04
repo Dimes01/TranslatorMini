ec58d8c46f91035ae18a731fc870466dd6e1a35f
Дмитрий Тяпин <tyapin2002@mail.ru>
2024-08-04 15:37:01 +0400

    feat: добавить документацию для классов-конфигов

--- src/main/java/com/example/tink_lab/configs/DatabaseConfig.java
+++ src/main/java/com/example/tink_lab/configs/DatabaseConfig.java
@@ -2,6 +2,14 @@ package com.example.tink_lab.configs;
 
 import org.springframework.context.annotation.Configuration;
 
+/**
+ * <b>Используется для конфигурации подключения к БД</b>
+ * <p>Сохраняется следующая информация:</p>
+ * <li>url (url для подключения к БД)</li>
+ * <li>user (имя пользователя)</li>
+ * <li>password (пароль пользователя)</li>
+ * <li>driverClassName (название драйвера БД для использования в JDBC)</li>
+ */
 @Configuration
 public class DatabaseConfig {
     public final String url = "jdbc:h2:mem:test";
--- src/main/java/com/example/tink_lab/configs/TranslationConfig.java
+++ src/main/java/com/example/tink_lab/configs/TranslationConfig.java
@@ -2,6 +2,14 @@ package com.example.tink_lab.configs;
 
 import org.springframework.context.annotation.Configuration;
 
+/**
+ * <b>Используется для конфигурации подключения к внешнему сервису перевода</b>
+ * <p>Сохраняется следующая информация:</p>
+ * <ul>
+ *     <li>url (url для подключения к сервису)</li>
+ * </ul>
+ * <b>!!!Для подключения именно к этому сервису никаких ApiKey не нужно</b>
+ */
 @Configuration
 public class TranslationConfig {
     public final String url = "https://ftapi.pythonanywhere.com/translate";
