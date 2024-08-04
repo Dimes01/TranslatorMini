0874ffe88b631419edabb2b7699c9ed67daaa78d
Дмитрий Тяпин <tyapin2002@mail.ru>
2024-08-04 15:19:50 +0400

    fix: изменить структуру конфигов

--- src/main/java/com/example/tink_lab/configs/DatabaseConfig.java
+++ src/main/java/com/example/tink_lab/configs/DatabaseConfig.java
@@ -1,24 +1,11 @@
 package com.example.tink_lab.configs;
 
-import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class DatabaseConfig {
-    private String url = "jdbc:h2:mem:test";
-    private String user = "user";
-    private String password = "password";
-    private String driverClassName = "org.h2.Driver";
-
-    public String GetURL() { return url; }
-    public void SetURL(String url) { this.url = url; }
-
-    public String GetUser() { return user; }
-    public void SetUser(String user) { this.user = user; }
-
-    public String GetPassword() { return password; }
-    public void SetPassword(String password) { this.password = password; }
-
-    public String GetDriverClassName() { return driverClassName; }
-    public void SetDriverClassName(String driverClassName) { this.driverClassName = driverClassName; }
+    public final String url = "jdbc:h2:mem:test";
+    public final String user = "user";
+    public final String password = "password";
+    public final String driverClassName = "org.h2.Driver";
 }
--- src/main/java/com/example/tink_lab/configs/TranslationConfig.java
+++ src/main/java/com/example/tink_lab/configs/TranslationConfig.java
@@ -1,13 +1,8 @@
 package com.example.tink_lab.configs;
 
-import org.springframework.boot.context.properties.ConfigurationProperties;
-import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class TranslationConfig {
-    private String url = "https://ftapi.pythonanywhere.com/translate";
-
-    public String getUrl() { return url; }
-    public void setUrl(String url) { this.url = url; }
+    public final String url = "https://ftapi.pythonanywhere.com/translate";
 }
