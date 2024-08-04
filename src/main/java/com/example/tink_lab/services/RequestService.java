0e7b25858dc203c671553b32c50dd04472faecf6
Дмитрий Тяпин <tyapin2002@mail.ru>
2024-08-04 16:27:09 +0400

    feat: добавить документацию для сервиса запросов

--- src/main/java/com/example/tink_lab/services/RequestService.java
+++ src/main/java/com/example/tink_lab/services/RequestService.java
@@ -4,14 +4,35 @@ import com.example.tink_lab.models.RequestLog;
 import com.example.tink_lab.repositories.RequestRepository;
 import org.springframework.stereotype.Service;
 
+/**
+ * <b>Используется для взаимодействия с репозиторием</b>
+ * <p>Имеет следующий функционал:</p>
+ * <ul>
+ *     <li>Создание лога запроса</li>
+ *     <li>Отправка лога в репозиторий</li>
+ * </ul>
+ */
 @Service
 public class RequestService {
+    /**
+     * Репозиторий для сохранения логов
+     */
     private final RequestRepository repository;
 
+    /**
+     * Инициализирует репозиторий
+     * @param repository Репозиторий для сохранения логов
+     */
     public RequestService(RequestRepository repository) {
         this.repository = repository;
     }
 
+    /**
+     * Создаёт лог и отправляет его в репозиторий
+     * @param ip IP-адрес запроса
+     * @param sourceText Текст на исходном языке
+     * @param translatedText Переведенный текст
+     */
     public void SaveRequest(String ip, String sourceText, String translatedText) {
         RequestLog log = new RequestLog(sourceText, translatedText, ip);
         repository.SaveRequest(log);
