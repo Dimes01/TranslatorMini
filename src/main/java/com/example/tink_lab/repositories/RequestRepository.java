6fcdf09ce2357cdcb49090bae4d6c9ec786e70b1
Dimes01 <tyapin2002@mail.ru>
2024-08-04 22:00:14 +0400

    feat: добавить метод для получения всех логов запросов из БД в RequestRepository

--- src/main/java/com/example/tink_lab/repositories/RequestRepository.java
+++ src/main/java/com/example/tink_lab/repositories/RequestRepository.java
@@ -4,10 +4,8 @@ import com.example.tink_lab.configs.DatabaseConfig;
 import com.example.tink_lab.models.RequestLog;
 import org.springframework.stereotype.Repository;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.PreparedStatement;
-import java.sql.SQLException;
+import java.sql.*;
+import java.util.LinkedList;
 
 /**
  * <b>Используется для обращения к одной конкретной базе данных</b>
@@ -23,19 +21,27 @@ import java.sql.SQLException;
  */
 @Repository
 public class RequestRepository {
+    private Connection conn;
+
     /**
      * Подготовленное выражение для создания в БД записи о запросе
      */
     private PreparedStatement addStatement;
 
     /**
+     * Выражение для получения всех логов запросов
+     */
+    private Statement selectAllStatement;
+
+    /**
      * Конструктор при создании объекта также создает подключение и стандартную таблицу
      */
     public RequestRepository() {
+        //TODO: Исправить, чтобы при ошибке подключения объект репозитория не создавался
         try {
             DatabaseConfig dbConf = new DatabaseConfig();
             Class.forName(dbConf.driverClassName).getDeclaredConstructor().newInstance();
-            Connection conn = DriverManager.getConnection(dbConf.url, dbConf.user, dbConf.password);
+            conn = DriverManager.getConnection(dbConf.url, dbConf.user, dbConf.password);
             var createStatement = conn.createStatement();
             createStatement.execute("CREATE TABLE requests (" +
                     "id INT PRIMARY KEY AUTO_INCREMENT," +
@@ -43,6 +49,7 @@ public class RequestRepository {
                     "source_text TEXT," +
                     "translated_text TEXT)");
             addStatement = conn.prepareStatement("INSERT INTO requests (ip_address, source_text, translated_text) VALUES (?, ?, ?)");
+            selectAllStatement = conn.createStatement();
         }
         catch(Exception ex) {
             System.out.println("Connection failed...");
@@ -55,6 +62,7 @@ public class RequestRepository {
      * @param requestLog Лог запроса на перевод
      */
     public void SaveRequest(RequestLog requestLog) {
+        // TODO: исправить так, чтобы при неудачной попытке добавления выбрасывало ошибку
         try {
             addStatement.setString(0, requestLog.GetIP());
             addStatement.setString(1, requestLog.GetSourceText());
@@ -63,5 +71,26 @@ public class RequestRepository {
         } catch (SQLException e) {
             System.out.println("Save request failed...");
         }
+    }
+
+    /**
+     * <b>Возвращает список логов всех запросов из БД.</b>
+     * <p>NOTE! Пустой список возвращается либо при отсутствии данных, либо при ошибке</p>
+     * @return Список всех запросов.
+     */
+    public LinkedList<RequestLog> GetAllRequests() {
+        var list = new LinkedList<RequestLog>();
+        try {
+            var resultSet = selectAllStatement.executeQuery("SELECT * FROM requests");
+            while (resultSet.next()) {
+                var ip = resultSet.getString(1);
+                var sourceText = resultSet.getString(2);
+                var translatedText = resultSet.getString(3);
+                list.add(new RequestLog(sourceText, translatedText, ip));
+            }
+        } catch (SQLException e) {
+            System.out.println("Select requests failed...");
+        }
+        return list;
     }
 }
