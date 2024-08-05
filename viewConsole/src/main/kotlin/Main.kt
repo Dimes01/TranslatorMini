fc8e83c975601a8c941c1df9336fce0d6c2a9444
Dimes01 <tyapin2002@mail.ru>
2024-08-05 23:45:39 +0400

    feat: добавить консольный клиент

--- viewConsole/src/main/kotlin/Main.kt
+++ viewConsole/src/main/kotlin/Main.kt
@@ -1,5 +1,8 @@
 package com.example
 
+import com.example.models.Request
+import com.example.models.Response
+import com.example.models.Language
 import io.ktor.client.*
 import io.ktor.client.call.*
 import io.ktor.client.engine.cio.*
@@ -23,43 +26,12 @@ fun main() {
     while (true) {
         print("Select command: ")
         val key = readln()
-        if (key == "-h") {
-            printInfo()
-        }
-        else if (key == "-t") {
-            var isTranslate = true
-            do {
-                print("Enter a text to translate: ")
-                val text = readln()
-                print("Enter a code of source language: ")
-                val sourceLang = readln()
-                print("Enter a code of target language: ")
-                val targetLang = readln()
-                val requestData = Request(text, sourceLang, targetLang)
-                runBlocking {
-                    try {
-                        val response: HttpResponse = client.post("http://localhost:8080/translate") {
-                            contentType(ContentType.Application.Json)
-                            setBody(requestData)
-                        }
-                        val responseBody: Response = response.body()
-                        println("Translated Text: ${responseBody.translatedText}")
-                    } catch (e: Exception) {
-                        println("Error: ${e.message}")
-                    }
-                }
-                print("Would you like to break operation? (Y/n or another text)")
-                val confirm = readln()
-                if (confirm == "Y") {
-                    isTranslate = false;
-                }
-            } while (isTranslate)
-        } else if (key == "-r") {
-            break
-        }
-        else {
-            println("Incorrect command!")
-        }
+        if (key == "-h") printInfo()
+        else if (key == "-t") postTranslate(client)
+        else if (key == "-l") getPossibleLanguages(client)
+        else if (key == "-s") getLogs(client)
+        else if (key == "-r") break
+        else println("Incorrect command!")
     }
 
     client.close()
@@ -68,5 +40,66 @@ fun main() {
 fun printInfo() {
     println("-h - help")
     println("-t - translate")
+    println("-l - possible languages")
+    println("-s - show logs (previous translates)")
     println("-r - return")
+}
+
+fun getPossibleLanguages(client: HttpClient) {
+    runBlocking {
+        try {
+            val responseBody: List<Language> = client.get("http://localhost:8080/translate/languages").body()
+            for (lang in responseBody) {
+                println("${lang.code} - ${lang.fullName}")
+            }
+        } catch (e: Exception) {
+            println("Error: ${e.message}")
+        }
+    }
+}
+
+fun postTranslate(client: HttpClient) {
+    var isTranslate = true
+    do {
+        print("Enter a text to translate: ")
+        val text = readln()
+        print("Enter a code of source language: ")
+        val sourceLang = readln()
+        print("Enter a code of target language: ")
+        val targetLang = readln()
+        val requestData = Request(text, sourceLang, targetLang)
+        runBlocking {
+            try {
+                val response: HttpResponse = client.post("http://localhost:8080/translate") {
+                    contentType(ContentType.Application.Json)
+                    setBody(requestData)
+                }
+                val responseBody: Response = response.body()
+                println("Translated Text: ${responseBody.translatedText}")
+            } catch (e: Exception) {
+                println("Error: ${e.message}")
+            }
+        }
+        print("Would you like to break operation? (Y/n or another text)")
+        val confirm = readln()
+        if (confirm == "Y") {
+            isTranslate = false;
+        }
+    } while (isTranslate)
+}
+
+fun getLogs(client: HttpClient) {
+    runBlocking {
+        try {
+            val responseBody: List<Response> = client.get("http://localhost:8080/translate/get").body()
+            println("----")
+            for (log in responseBody) {
+                println("Source text: ${log.sourceText}")
+                println("Translated text: ${log.translatedText}")
+                println("----")
+            }
+        } catch (e: Exception) {
+            println("Error: ${e.message}")
+        }
+    }
 }
